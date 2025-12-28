b9203c9494b8db4426f7ad80c41ebb5c44425f2e
Nitish Sharma <ernitishsharma361@gmail.com>
2025-12-28 09:21:20 +0530

    ui works

--- folderScture/src/App.jsx
+++ folderScture/src/App.jsx
@@ -15,38 +15,48 @@ const App = () => {
     setnumber("")
   }
   return (
-    <div>
-      <h1>Add your details in the form</h1>
-      <form onSubmit={(e) =>
-         submitbtn(e)
-        }>
-        <input 
-          type="text" 
-          placeholder='Enter Your Name'
-          value={name}
-          onChange={(e) =>
-            setname(e.target.value)
-          } 
-          required 
-        />
-        <input
-          type="number" 
-          required 
-          placeholder='Enter Your Number'
-          value={number}
-          onChange={(e) =>
-          setnumber(e.target.value)
-         } 
-         />
-        <button>Submit</button>
-      </form>
-      {newuser.map((item, index) => (
-        <div key={index}>
-          <h2>Your Name is: {item.name}</h2>
-          <h2>Your Number is: {item.number}</h2>
-        </div>
-      ))
-      }
+    <div className='contactform'>
+      <div className="adduser">
+        <h1>Add Contact</h1>
+        <form onSubmit={(e) =>
+           submitbtn(e)
+          }>
+          <p>Name</p>
+          <input 
+            type="text" 
+            placeholder='Enter Your Name'
+            value={name}
+            onChange={(e) =>
+              setname(e.target.value)
+            } 
+            required 
+          />
+          <p>Number</p>
+          <input
+            type="number" 
+            required 
+            placeholder='Enter Your Number'
+            value={number}
+            onChange={(e) =>
+            setnumber(e.target.value)
+           } 
+           />
+          <button>Submit</button>
+        </form>
+      </div>
+      <div className="userlist">
+        <h1>Your Contacts</h1>
+        {newuser.map((item, index) => (
+          <div className="userbox">
+            <i class="ri-user-line"></i>
+            <div key={index} className="userdetails">
+              <h2>{item.name}</h2>
+              <h2>{item.number}</h2>
+            </div>
+            <i class="ri-information-line"></i>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
